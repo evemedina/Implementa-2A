beda10f43b1b0ee4a266a502b70db5b662be6e79
evemedina <evelynalvaradomedina.2008@gmail.com>
2025-03-21 13:07:00 -0600

    Add files via upload

---
+++ Programa 3.py
@@ -0,0 +1,7 @@
+p=int(input("Numero de payasos:"))
+multiplicacion1=p*112
+print ("Peso total del paquete:",multiplicacion1)
+m=int(input("Numero de muñecas:"))
+multiplicacion2=m*75
+print ("Peso total del paquete:",multiplicacion2)
+#num no solo se puede usar como una variable esta se puede ca,mbiar como en este caso p representa a los payasos.
